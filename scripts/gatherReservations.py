0b6f87254d24196fcf3b3a7b5cbf30e389e587b7
DanyWerfy <Dany@Werfy.com>
2025-08-25 11:07:23 -0400

    refactored code to be more resuable and added more comments

--- scripts/gatherReservations.py
+++ scripts/gatherReservations.py
@@ -18,52 +18,74 @@ propertyIDs =[
     "317876",
     "318157"
 ]
-def getReservations():
-    outputPath = "../data/reservations.json"
-    # clear the json
-    with open (outputPath, "w", encoding="utf-8", newline="") as f:
-        pass
-    baseUrl = "https://api.cloudbeds.com/api/v1.3/"
-    endpoint = "getReservation"
-    additionalEndPoint = "sWithRateDetails"
 
+def getReservations():
+    # create endpoint
+    baseUrl = "https://api.cloudbeds.com/api/v1.3/"
+    baseEndpoint = "getReservation"
+    additionalEndPoint = "sWithRateDetails"
+    endPoint = baseEndpoint + additionalEndPoint
+    fullEndPoint = f"{baseUrl}{endPoint}"
+
+    # pass in headers
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "accept": "application/json"
     }
 
-    initParams = {
-        "propertyID": propertyIDs[0],
-        "resultsFrom" : date(2020,1,1)
+    outputPath = "../data/reservations.json"
 
-    }
-    finalEndPoint = endpoint + additionalEndPoint
-    fullURL = f"{baseUrl}{finalEndPoint}"
-    response = requests.get(url=fullURL, headers=headers, params=initParams)
-    resJSON = response.json()
-    totalCount = resJSON["total"]
-    countPerPage = resJSON["count"]
-    print(f"{totalCount} total entries")
-    print(f"{countPerPage} per page")
-    loops = math.ceil(totalCount/countPerPage)
+    # clear the json
+    with open (outputPath, "w", encoding="utf-8", newline="") as f:
+        pass
+
+    # figure out how many calls we need
+    loops = initalCall(fullEndPoint,headers)
+
     allReservations = []
     for i in range(0,loops):
+        # params to be sent to the call
         params = {
             "propertyID": propertyIDs[0],
             "pageNumber": i + 1,
             "includeAllRooms" : True,
             "resultsFrom" : date(2020,1,1)
         }
-        response = requests.get(url=fullURL, headers=headers, params=params)
-        resJSON = response.json()
-        if resJSON["success"] != True:
-             print(f"something went wrong")
-             return
-        data = resJSON["data"]
+        data = apiCall(fullEndPoint=fullEndPoint,headers=headers,params=params)
         print(f"\r{i}/{loops} pages complete        ", end="")
         allReservations.extend(data)
     with open (outputPath, "a", encoding="utf-8", newline="") as f:
             json.dump(allReservations, f, indent = 4)
+
+
+# this is the intial API call to figure out how many calls we need in total
+# returns (total count, count per page)
+def initalCall(fullEndPoint, headers):
+    # pass in inital parameters
+    initParams = {
+        "propertyID": propertyIDs[0],
+        "resultsFrom" : date(2020,1,1)
+    }
+    # grab response and convert to JSON
+    response = requests.get(url=fullEndPoint, headers=headers, params=initParams)
+    resJSON = response.json()
+    totalCount = resJSON["total"]
+    countPerPage = resJSON["count"]
+    print(f"{totalCount} total entries")
+    print(f"{countPerPage} per page")
+    # take the ceiling to ensure we dont miss any data
+    loops = math.ceil(totalCount/countPerPage)
+
+    return loops
+
+def apiCall(fullEndPoint,headers, params):
+    response = requests.get(url=fullEndPoint, headers=headers, params=params)
+    resJSON = response.json()
+    if resJSON["success"] != True:
+            print(f"something went wrong")
+            return
+    data = resJSON["data"]
+    return data
 
 def testBlockedDates():
     outputPath = "../data/blockedDates.json"
